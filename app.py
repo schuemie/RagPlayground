133878ad978f6cd9208533a3a2dbe02be591120e
schuemie <schuemie@ohdsi.org>
2024-04-03 16:10:02 +0200

    Adding logo

--- app.py
+++ app.py
@@ -1,14 +1,43 @@
+from datetime import date
+import random
+from pathlib import Path
+
 from shiny import App, Inputs, Outputs, Session, render, ui, reactive
+from shiny.types import ImgData
+
 import app_functions
 
+
+template = """
+You are a pharmaceutical research & development professional performing the structured benefit-risk analysis for a new investigational drug.
+Use only the provided pieces of retrieved context to perform the requested tasks. 
+
+Tasks: 
+1. Write the 'Analysis of Condition' section on the evidence and uncertainties for the nature of {indication}, focusing on the most important aspects of the condition that {product} may target, symptom burden, and natural course of the disease. What aspects of the condition have the most impact on the population with the condition?  Provide the most critical information on incidence, duration, disease progression, morbidity, symptoms, impact on patient functioning, mortality, health-related quality of life, and important differences in outcome or severity in subpopulations. For each point of information you provide, reference what document and page number this information was taken from.
+2. Summarize in no more than 3 bullets the key conclusions of the information you just provided.
+"""
+
 app_ui = ui.page_fluid(
-    ui.panel_title("RAG test app"),
-    ui.input_text_area("prompt", "Prompt:", "What is the primary treatment option for multiple myeloma?"),
+    ui.row(
+        ui.column(11, ui.panel_title("RAG test app")),
+        ui.column(1, ui.output_image("image", width="100px", height="97px"), align="right")
+    ),
+    ui.layout_column_wrap(
+        ui.input_text("indication", "Indication:", "Early Onset Severe Hemolytic Disease of the Fetus and Newborn (EOS-HDFN)"),
+        ui.input_text("product", "Product:", "nipocalimab")
+    ),
+    ui.input_text_area("template",
+                       "Prompt template:",
+                       template,
+                       width="100%",
+                       height="200px"),
+    ui.input_numeric("k", "Number of text chunks to retrieve:", 3),
     ui.input_action_button("submit", "Submit"),
+    ui.p("Instantiated prompt with context:"),
+    ui.pre(ui.output_text("prompt_with_context"), style="white-space: pre-wrap; word-break: keep-all;"),
     ui.p("Response:"),
     ui.pre(ui.output_text("response"), style="white-space: pre-wrap; word-break: keep-all;"),
-    ui.p("Prompt with context:"),
-    ui.pre(ui.output_text("prompt_with_context"), style="white-space: pre-wrap; word-break: keep-all;"),
+    ui.download_button("download_data", "Download")
 )
 
 
@@ -20,7 +49,8 @@ def server(input: Inputs, output: Outputs, session: Session):
     @reactive.event(input.submit)
     async def _():
         id = ui.notification_show("Processing...", duration=999, type="message", close_button=False)
-        value.set(await app_functions.get_llm_response(input.prompt()))
+        prompt = input.template().format(indication=input.indication(), product=input.product())
+        value.set(await app_functions.get_llm_response(prompt, input.k()))
         ui.notification_remove(id)
 
     @render.text
@@ -30,6 +60,29 @@ def server(input: Inputs, output: Outputs, session: Session):
     @render.text
     def prompt_with_context():
         return value.get().prompt
+
+    @render.download(
+        filename=lambda: f"brPrompts-{date.today().isoformat()}-{random.randint(100, 999)}.txt"
+    )
+    async def download_data():
+        yield "\nIndication:\n"
+        yield input.indication() + "\n"
+        yield "\nProduct:\n"
+        yield input.product() + "\n"
+        yield "Template:\n"
+        yield input.template() + "\n"
+        yield "\nNumber of text chunks to retrieve:\n"
+        yield str(input.k()) + "\n"
+        yield "\nPrompt with context:\n"
+        yield value.get().prompt + "\n"
+        yield "\nResponse:\n"
+        yield value.get().response + "\n"
+
+    @render.image
+    def image():
+        dir = Path(__file__).resolve().parent
+        img: ImgData = {"src": str(dir / "logo.png"), "width": "100px"}
+        return img
 
 
 app = App(app_ui, server, debug=False)
--- app_functions.py
+++ app_functions.py
@@ -4,8 +4,6 @@ from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
-from langchain_openai import AzureChatOpenAI
-from langchain_core.messages import HumanMessage
 from langchain_community.llms import LlamaCpp
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
@@ -14,41 +12,28 @@ import os
 import asyncio
 from dataclasses import dataclass
 
-if os.environ.get("genai_gpt4_endpoint", "default") == "default":
-    # For local testing: use LlamaCpp running Zephyr and Jina embeddings on the local machine.
-    use_gpt4 = False
-    model_path = "/Users/schuemie/git/LocalLlm2/models/zephyr-7b-beta.Q5_K_M.gguf"
-    llm = LlamaCpp(model_path=model_path,
-                   n_gpu_layers=-1,
-                   n_threads=8,
-                   n_ctx=4096,
-                   use_mlock=True)
-    model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
-    embeddings = HuggingFaceEmbeddings(model_name="jinaai/jina-embeddings-v2-base-en")
-    db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
-    retriever = db.as_retriever(search_kwargs={"k": 2})
-    template = """<|system|>\n</s>\n</|user|>user
-    Answer the question based only on the following context:
-    {context}
+model_path = "/Users/schuemie/git/LocalLlm2/models/zephyr-7b-beta.Q5_K_M.gguf"
+llm = LlamaCpp(model_path=model_path,
+               n_gpu_layers=-1,
+               n_threads=8,
+               n_ctx=4096,
+               use_mlock=True)
+model = AutoModel.from_pretrained('jinaai/jina-embeddings-v2-base-en', trust_remote_code=True)
+embeddings = HuggingFaceEmbeddings(model_name="jinaai/jina-embeddings-v2-base-en")
+db = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
+retriever = db.as_retriever()
+template = """<|system|>\n</s>\n</|user|>
+{question}
 
-    Provide the source(s) of your answer.
+Context: {context}
+\n</s>\n<|assistant|>"""
+prompt = PromptTemplate(input_variables=["context", "question"], template=template)
+output_parser = StrOutputParser()
 
-    Question: {question}\n</s>\n<|assistant|>"""
-    prompt = PromptTemplate(input_variables=["context", "question"], template=template)
-    output_parser = StrOutputParser()
-
-    setup_and_retrieval = RunnableParallel(
-        {"context": retriever, "question": RunnablePassthrough()}
-    )
-    chain = setup_and_retrieval | prompt | llm | output_parser
-else:
-    # For production: use Azure OpenAI with GPT-4 and Azure embeddings.
-    use_gpt4 = True
-    llm = AzureChatOpenAI(
-        openai_api_key=os.environ.get("genai_api_key"),
-        api_version="2023-03-15-preview",
-        azure_endpoint=os.environ.get("genai_gpt4_endpoint"),
-    )
+setup_and_retrieval = RunnableParallel(
+    {"context": retriever, "question": RunnablePassthrough()}
+)
+chain = setup_and_retrieval | prompt | llm | output_parser
 
 
 class StoreLlmPromptHandler(BaseCallbackHandler):
@@ -68,20 +53,11 @@ class LlmResponse:
     response: str
 
 
-async def get_llm_response(prompt: str) -> LlmResponse:
-    if use_gpt4:
-        message = HumanMessage(
-            content=prompt
-        )
-        response = await llm.ainvoke([message])
-        return response.content
-    else:
-        # message = "<|system|>You are a helpful assistant.</s><|user|>" + prompt + "</s><|assistant|>"
-        # response = await llm.ainvoke(message)
-        # return response
-        handler = StoreLlmPromptHandler()
-        response = await chain.ainvoke(prompt, {"callbacks": [handler]})
-        return LlmResponse(prompt=handler.get_llm_prompt(), response=response)
+async def get_llm_response(prompt: str, k: int) -> LlmResponse:
+    handler = StoreLlmPromptHandler()
+    retriever.search_kwargs = {"k": k}
+    response = await chain.ainvoke(prompt, {"callbacks": [handler]})
+    return LlmResponse(prompt=handler.get_llm_prompt(), response=response)
 
 
 if __name__ == "__main__":
